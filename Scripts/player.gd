b158e1eeb049e4e6c2c3712f9164abd2a200d4b8
Kyle <kyruken@gmail.com>
2023-10-27 13:33:58 -0700

    add attack cooldown

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,7 +13,10 @@ var is_attentive: bool
 @onready var health_bar = $HealthBar
 @onready var attention_bar = $AttentionBar
 
+var can_shoot = true
+
 func _ready():
+	
 	is_attentive = true
 	health = max_health
 	attention = max_attention
@@ -29,10 +32,10 @@ func _physics_process(delta):
 	update_attribute(health_bar, health)
 	decrement_attention(attention_deplete)
 	update_attribute(attention_bar, attention)
-
+	
 func _unhandled_input(event: InputEvent):
 	if is_attentive:
-		if event.is_action_released("shoot"):
+		if event.is_action_released("shoot") and can_shoot:
 			shoot()
 	
 	if event.is_action_pressed("check_phone"):
@@ -48,15 +51,17 @@ func check_phone():
 	is_attentive = true
 	
 func shoot():
+	
 	$Sprite2D/AnimationPlayer.play("attack_animation") 
 	var bullet_instance = Bullet.instantiate()
 	bullet_instance.global_position = end_of_gun.global_position
 	var target = get_global_mouse_position()
-	
+
 	#We find the direction towards the mouse through vector functions
 	var direction_to_mouse = end_of_gun.global_position.direction_to(target).normalized()
 	bullet_instance.set_direction(direction_to_mouse)
 	emit_signal("player_fired_bullet", bullet_instance, end_of_gun.global_position, direction_to_mouse)
+	can_shoot = false
 
 
 func update_attribute(attribute_bar, amount):
@@ -76,5 +81,7 @@ func handle_hit():
 func die():
 	#get_parent gets top most node, then queue_free removes node from tree
 	get_parent().queue_free()
-	
-	
+
+
+func _on_timer_timeout():
+	can_shoot = true
