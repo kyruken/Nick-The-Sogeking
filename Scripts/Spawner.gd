ecdb717688c2968bb69b8f3618ad043fa94eb39d
Kyle <kyruken@gmail.com>
2023-11-04 16:09:26 -0700

    fix spawner

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var mob_to_spawn : PackedScene
 @onready var spawn_timer = $SpawnTimer
+@onready var tree = get_tree().get_root()
 
 var can_spawn = false
 
@@ -11,7 +12,8 @@ func _process(delta):
 
 func spawn():
 	var mob_instance = mob_to_spawn.instantiate()
-	add_child(mob_instance)
+	mob_instance.global_position = self.global_position
+	tree.add_child(mob_instance)
 	can_spawn = false
 	
 func _on_spawn_timer_timeout():
